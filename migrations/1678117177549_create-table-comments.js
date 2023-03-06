564b23f23fdbf827e63528c67022543fc1197d86
Ismail <ismailsmakhus@gmail.com>
2023-03-06 23:03:07 +0700

    create table comments

---
+++ migrations/1678117177549_create-table-comments.js
@@ -0,0 +1,37 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    is_delete: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: 'FALSE',
+    },
+  });
+
+  pgm.addConstraint('comments', 'fk_comments.thread_id_threads.id', 'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE');
+  pgm.addConstraint('comments', 'fk_comments.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comments');
+};
