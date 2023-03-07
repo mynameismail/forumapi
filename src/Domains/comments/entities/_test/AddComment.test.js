6724aba4f7fe83353f3724ea65254799522ac8e2
Ismail <ismailsmakhus@gmail.com>
2023-03-07 07:46:10 +0700

    comment domain

---
+++ src/Domains/comments/entities/_test/AddComment.test.js
@@ -0,0 +1,39 @@
+const AddComment = require('../AddComment');
+
+describe('an AddComment entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      content: 'abc',
+      threadId: 'abc',
+    };
+    // Action and Assert
+    expect(() => new AddComment(payload)).toThrowError('ADD_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      content: 123,
+      threadId: true,
+      owner: 'abc',
+    };
+    // Action and Assert
+    expect(() => new AddComment(payload)).toThrowError('ADD_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create addComment object correctly', () => {
+    // Arrange
+    const payload = {
+      content: 'abc',
+      threadId: 'abc',
+      owner: 'abc',
+    };
+    // Action
+    const { content, threadId, owner } = new AddComment(payload);
+    // Assert
+    expect(content).toEqual(payload.content);
+    expect(threadId).toEqual(payload.threadId);
+    expect(owner).toEqual(payload.owner);
+  });
+});
