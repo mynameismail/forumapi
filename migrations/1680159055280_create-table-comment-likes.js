66a3449fcb89f0c48a56b5de325e8492e3ff4608
Ismail <ismailsmakhus@gmail.com>
2023-03-30 13:54:13 +0700

    create table comment likes

---
+++ migrations/1680159055280_create-table-comment-likes.js
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('comment_likes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('comment_likes', 'unique_comment_id_and_user_id', 'UNIQUE(comment_id, user_id)');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comment_likes');
+};
